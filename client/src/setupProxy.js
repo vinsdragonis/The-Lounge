25cb94425664069515eb0e4e485d365f4194f1b0
vinsdragonis <zrexteam128@gmail.com>
2021-12-16 23:32:30 +0530

    Update setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,14 +1,14 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
+// import { createProxyMiddleware } from 'http-proxy-middleware';
 
-module.exports = function(app) {
-  app.use(
-    '/server',
-    createProxyMiddleware({
-      target: 'http://localhost:8800',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/server/settings': '/server/app/settings'
-      },
-    })
-  );
-};
+// module.exports = function(app) {
+//   app.use(
+//     '/server',
+//     createProxyMiddleware({
+//       target: 'http://localhost:8800',
+//       changeOrigin: true,
+//       // pathRewrite: {
+//       //   '^/server/settings': '/server/app/settings'
+//       // },
+//     })
+//   );
+// };
