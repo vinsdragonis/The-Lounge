1eb5ef3654d767a521aab119b8599a9ab70fb354
vinsdragonis <zrexteam128@gmail.com>
2022-01-07 18:41:51 +0530

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
     name: {
         type: String,
         required: true,
@@ -51,3 +51,5 @@ const userSchema = new Schema({
 }, {
     timestamps: true
 });
+
+module.exports = mongoose.model('User', UserSchema);
