3688e7d90339b5e59a19d755dc42b8983b1ba869
vinsdragonis <zrexteam128@gmail.com>
2022-01-08 09:59:20 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    env: {
+        CLOUDINARY_URL: "https://api.cloudinary.com/v1/dragonis/image/upload"
+        // CLOUDINARY_URL: "cloudinary://311237865561142:psBRIrDusd5lzfrje03nM9XmVs8@dragonis/image/upload"
+    }
+}
