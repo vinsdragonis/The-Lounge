11631b0e9c0f8598468e1263a2fe09df00c9a89b
vinsdragonis <zrexteam128@gmail.com>
2022-01-23 15:03:27 +0530

    .

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import baseUrl from '../utils/baseUrl';
 
-export default function index() {
+export default function index({ user, userFollowStats }) {
+    console.log(user, userFollowStats);
     return (
         <div>
             Homepage
